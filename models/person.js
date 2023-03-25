b9ca9b1428c23c42cb6c7e847cb4275a92746601
Shane Arland <arlandshane@gmail.com>
2023-03-25 17:15:28 +0530

    made name and message required

--- models/person.js
+++ models/person.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const PersonsSchema = new Schema({
-	name: { type: String },
-	message: { type: String },
+	name: { type: String, required: true },
+	message: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Person", PersonsSchema);
