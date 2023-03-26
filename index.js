bbaf758bad027c81be4a623217d12f6f60767f07
Shane Arland <arlandshane@gmail.com>
2023-03-26 21:54:54 +0530

    fixed minor bug

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 const ejs = require("ejs");
 const Person = require("./models/person");
-const User = require("./models/user");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,6 +21,7 @@ const connectDB = async () => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+<<<<<<< HEAD
 app.use(
 	session({
 		secret: process.env.SECRET_KEY,
@@ -30,6 +29,8 @@ app.use(
 		saveUninitialized: false,
 	})
 );
+=======
+>>>>>>> parent of 7c578e8 (allowed users to register and login)
 
 app.get("/", async (req, res) => {
 	const persons = await Person.find();
@@ -39,9 +40,7 @@ app.get("/", async (req, res) => {
 		(err, html) => {
 			if (err) {
 				console.log(err);
-				res.status(500).send(
-					"<h1>Error: 500</h1><p>Error rendering template</p>"
-				);
+				res.status(500).send("Error rendering template");
 			} else {
 				res.send(html);
 			}
@@ -49,66 +48,28 @@ app.get("/", async (req, res) => {
 	);
 });
 
-app.get("/register", (req, res) => {
-	res.sendFile(__dirname + "/register.html");
-});
-
-app.get("/login", (req, res) => {
-	res.sendFile(__dirname + "/login.html");
-});
-
 app.post("/", async (req, res) => {
-	const { message } = req.body;
-	const name = req.session.username; // use the username from the session
+	const { name, message } = req.body;
 	try {
 		const person = new Person({ name, message });
 		await person.save();
 		res.redirect("/");
 	} catch (error) {
 		console.log(error);
-		res.status(500).send(
-			"<h1>Error: 500</h1><p>Error adding message. You need to be connected to the internet and <span><a href='/login'>logged in</a></span></p>"
-		);
+		res.status(500).send("Error adding message");
 	}
 });
 
-app.post("/login", async (req, res) => {
-	const { emailOrUsername, password } = req.body;
-	try {
-		const user = await User.findOne({
-			$or: [{ email: emailOrUsername }, { username: emailOrUsername }],
-		});
-		if (user && user.password === password) {
-			req.session.username = user.username; // save the username to the session
-			res.redirect("/");
-		} else {
-			res.status(401).send("Invalid credentials");
-		}
-	} catch (error) {
-		console.log(error);
-		res.status(500).send("<h1>Error:500</h1><p>Error logging in</p>");
-	}
-});
-
-app.post("/register", async (req, res) => {
-	const { email, username, password } = req.body;
-	try {
-		const user = new User({ email, username, password });
-		await user.save();
-		req.session.username = user.username; // save the username to the session
-		res.redirect("/");
-	} catch (error) {
-		console.log(error);
-		res.status(500).send(
-			"<h1>Error: 500</h1><p>Error registering user.<br>You could already be registered, try <a href='/login'>login</a></p>"
-		);
-	}
-});
-
-app.get("/logout", (req, res) => {
-	req.session.destroy();
-	res.redirect("/login");
-});
+// app.post("/delete-database", async (req, res) => {
+// 	try {
+// 		await Book.deleteMany();
+// 		console.log("Database deleted");
+// 		res.redirect("/");
+// 	} catch (error) {
+// 		console.log(error);
+// 		res.status(500).send("Error deleting database");
+// 	}
+// });
 
 connectDB().then(() => {
 	app.listen(PORT, () => {
