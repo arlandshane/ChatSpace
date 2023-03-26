fe1b5e30e6ee85db19fe36f22e5ec9627d39068b
Shane Arland <arlandshane@gmail.com>
2023-03-26 21:55:45 +0530

    fixed minor bug

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -21,16 +22,13 @@ const connectDB = async () => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-<<<<<<< HEAD
 app.use(
 	session({
-		secret: process.env.SECRET_KEY,
+		secret: "shane_arland_secret_session_key",
 		resave: false,
 		saveUninitialized: false,
 	})
 );
-=======
->>>>>>> parent of 7c578e8 (allowed users to register and login)
 
 app.get("/", async (req, res) => {
 	const persons = await Person.find();
@@ -59,17 +57,6 @@ app.post("/", async (req, res) => {
 		res.status(500).send("Error adding message");
 	}
 });
-
-// app.post("/delete-database", async (req, res) => {
-// 	try {
-// 		await Book.deleteMany();
-// 		console.log("Database deleted");
-// 		res.redirect("/");
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(500).send("Error deleting database");
-// 	}
-// });
 
 connectDB().then(() => {
 	app.listen(PORT, () => {
